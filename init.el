52163655556646b840082b09cc1d08fc2d5cdb35
leandrusfanzec <lucasc.fernandez@tutanota.com>
2023-02-14 21:17:35 -0300

    primer commit

--- init.el
+++ init.el
@@ -1,22 +1,3 @@
 (require 'org)
-(org-babel-load-file (expand-file-name "/home/lucas/.emacs.d/config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c505ae23385324c21821b24c9cc1d68d8da6f3cfb117eb18826d146b8ec01b15" "bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" default))
- '(dired-sidebar-width 20)
- '(olivetti-body-width 62)
- '(org-gtd-mode t)
- '(package-selected-packages
-   '(rainbow-mode denote org-preview-html elfeed org-gtd dired-sidebar org-noter pdf-tools window org-present visual-fill-column org-roam-ui olivetti toc-org consult orderless vertico use-package))
- '(smtpmail-smtp-server "smtp.google.com" t)
- '(smtpmail-smtp-service 25 t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file (expand-file-name "/home/$USER/.emacs.d/README.org"))
+
