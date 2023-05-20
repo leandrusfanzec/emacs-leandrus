465dae3684170d1444769b5f68ad9f11e6a5385a
leandrusfanzec <lucasc.fernandez@tutanota.com>
2023-05-20 09:50:24 -0400

    retira pdf-tools y modifica README

--- init.el
+++ init.el
@@ -1,15 +1,3 @@
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(pdf-tools solar vertico use-package orderless)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
