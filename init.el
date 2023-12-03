b851454002cbe6e261ac30b74924b4320298a578
lucas <lucas@localhost.localdomain>
2023-12-03 15:58:03 -0300

    incluye use-package en el orden

--- init.el
+++ init.el
@@ -6,6 +6,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8122fb61548fe36171d9cf24cdb9b5f24d053b626d4cda739c3815e080623209" "d8bcb88ef0a3259a38d6deba78e569c0750ebfede82ad3e6da16573419fef48c" default))
+ '(delete-selection-mode nil)
+ '(flyspell-default-dictionary nil)
+ '(ispell-alternate-dictionary nil)
+ '(ispell-dictionary "spanish")
  '(neo-smart-open t)
  '(neo-theme 'ascii)
  '(package-selected-packages
